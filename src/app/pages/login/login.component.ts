a7c78d4200b8df7a77e62482c829267e46c7fc2e
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2024-04-26 16:03:02 -0300

    add: handler for when the server is asleep

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,11 +2,17 @@ import { Component, OnInit, inject } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DefaultLayoutComponent } from '../../components/default-layout/default-layout.component';
-import { MessageComponent } from "../../components/message/message.component";
+import { MessageClass, MessageComponent } from "../../components/message/message.component";
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { LoginService } from '../../services/login.service';
 import { AuthResponse } from '../../types/auth-response.type';
 import { HttpErrorResponse } from '@angular/common/http';
+
+export interface Feedback {
+  show: boolean;
+  type: MessageClass;
+  message: string;
+}
 
 @Component({
     selector: 'app-login',
@@ -23,8 +29,11 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-  error: boolean = false;
-  errorMessage: string = '';
+  feedback: Feedback = {
+    message: '',
+    type: 'error',
+    show: false,
+  }
 
   router = inject(Router);
   service = inject(LoginService);
@@ -45,7 +54,7 @@ export class LoginComponent implements OnInit {
   }
 
   submit() {
-    this.error = false;
+    this.resetFeedback();
 
     if (this.loginForm.invalid) {
       return;
@@ -60,6 +69,23 @@ export class LoginComponent implements OnInit {
   }
 
 
+  private resetFeedback() {
+    this.feedback = {show: false, type: 'error', message: ''};
+  }
+
+  private setShow(show: boolean) {
+    this.feedback.show = show;
+  }
+
+  private setMessage(message: string) {
+    this.feedback.message = message;
+  }
+
+  private setType(type: MessageClass) {
+    this.feedback.type = type;
+  }
+
+
   private handleSuccess(response: AuthResponse) {
     console.log('Login feito com sucesso');
     console.log(response);    
@@ -68,12 +94,23 @@ export class LoginComponent implements OnInit {
   handleError(err: HttpErrorResponse) {
 
     switch(err.status) {
-        case 0: this.errorMessage = 'The server may be offline, try again later.'; break;
-        case 409: this.errorMessage = "This email is already registered."; break;
-        default: this.errorMessage = "The server is waking up, try again in a few seconds."; break;
+        case 0: 
+          this.setMessage('The server may be offline, try again later.');
+          break;
+        case 409: 
+          this.setMessage('This email is already registered.'); 
+          break;
+        case 401: 
+          this.setMessage('The password and/or email are invalid.');     
+          break;
+        default: 
+          this.setMessage('The server is waking up, try again in a few seconds.');
+          this.setType('info');
+          break;
     }
     
-    this.error = !!err;
+    this.setShow(!!err);
+
 }
 
   private resetForm() {
